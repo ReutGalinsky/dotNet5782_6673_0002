0e76c7005972ba0cc6aada15339a2a98608bcb34
ReutGalinsky <rgalinsk@g.jct.ac.il>
2021-10-07 09:54:18 +0300

    Initial code and welcome functions

---
+++ Targil0/Program6673.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome6673();
+            Welcome0002();
+            Console.ReadKey();
+
+        }
+        static partial void Welcome0002();
+        private static void Welcome6673()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
