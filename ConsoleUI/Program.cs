5ac0f4c2ddd57a7bbec126456e0dfedbdbc2c64b
ReutGalinsky <rgalinsk@g.jct.ac.il>
2021-10-24 17:10:03 +0300

    add little menue

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -51,13 +51,43 @@ namespace ConsoleUI
                                 default:
                                     break;
                             }
-                      
                         break;
                     case Options.Updating:
                         Console.WriteLine("1. match parcel to a drone \n 2.collecting parcel by a drone \n 3.giving parcel to a customer \n 4.sending a dorne to be charged \n 5. release drone from charging");
+                        specialChoose = (Update)Console.Read();
+                        switch (specialChoose)
+                        {
+                            case Update.Match:
+                                break;
+                            case Update.Collect:
+                                break;
+                            case Update.Giving:
+                                break;
+                            case Update.Sending:
+                                break;
+                            case Update.Release:
+                                break;
+                            default:
+                                break;
+                        }
                         break;
                     case Options.ShowItemp:
                         Console.WriteLine("1. show base station \n 2.show drone \n 3.show customer \n 4.show parcel");
+                        internalChoose = (States)Console.Read();
+                        switch (internalChoose)
+                        {
+                            case States.BaseStation:
+                                break;
+                            case States.Drone:
+                                break;
+                            case States.Customer:
+                                break;
+                            case States.Parcel:
+                                break;
+                            case States.Unmatched:
+                            default:
+                                break;
+                        }
                         break;
                     case Options.ShowList:
                         Console.WriteLine("1. show the list of base stations \n 2. show the list of the drones \n 3.shoe the list of the customer \n 4. shoe the list of the parcel \n 5. show the list of the unmatched percels \n 6. show the list of base stations with availible charge slots");
